53d53d52cfd850ffaca36c4151238ed391a6c0c6
Baig <baiga5@corp.nychhc.org>
2024-12-30 12:47:03 -0500

    Activation Function and mock forward pass

--- neuron.py
+++ neuron.py
@@ -6,25 +6,49 @@ nnfs.init()
 
 class Layer_Dense:
     def __init__(self,n_inputs,n_neurons):             # layer initialization
-        self.weights = 0.01 * np.random.randn(n_inputs,n_neurons)
-        self.biases = np.zeros((1,n_neurons))            # 2 parameters for np.zeros row x column
+        self.weights = 0.01 * np.random.randn(n_inputs,n_neurons)  #rows(input count) x columns(neuron count) -  0.01 to ensure minimal weight initialization
+        self.biases = np.zeros((1,n_neurons))            # 2 parameters for np.zeros row ( 1 row) x column (number of neurons)
  
     def forward(self, inputs):        # initialize forward pass 
-        self.output = np.dot(inputs,self.weights) + self.biases   
+        self.output = np.dot(inputs,self.weights) + self.biases     # Matrix Mult. of inputs against weights added with broadcasted biases
+
+class Activation_ReLU:       # ReLU to introduce non-linearity inbetween multiplication layers
+    def forward(self,inputs):   
+        self.output = np.maximum(0,inputs)   # same dimension as input matrix, replaces value with 0 if < 0
+
+class Activation_Softmax:     #SoftMax activation used for probability final activation layer (due to spiral classification problem )
+    def forward(self,inputs):     
+        # e^x conversion of inputs in the matrix, all subtracted by the largest value in order to taper magnitude 
+        exp_values = np.exp(inputs - np.max(inputs,axis=1,keepdims=True))     # axis = 1 returns a matrix #neurons x 1 of the max value per row 
+        # divide the e^x conversion by the summation of the row (each element in the row is a different neuron output) 
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims = True)   #axis = 1 to maintain #neurons x 1 dimensions 
+        self.output = probabilities
 
 X,y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2,3)
+
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3,3)
+
+activation2 = Activation_Softmax()
+#singular forward pass against 2 layers with one ReLU and a concluding softmax 
 dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-print(dense1.output[:5])
+#Non propogated biases and weights for this run 
+print(activation2.output[:5])
 
-plt.scatter(X[:,0], X[:,1], c=y, cmap='brg')
-plt.show()
+
+# plt.scatter(X[:,0], X[:,1], c=y, cmap='brg')
+# plt.show()
 
 # A = [[1,2,3],[4,155,6],[7,8,9]]
 # print(  np.max(A,axis=1))
- 
+   
 # biases = [2,3,0.5]
 # weights = [[0.2,0.8,-.5,1], [0.5,-0.91,0.26,-0.5], [-0.26,-0.27,0.17,0.87]]
 # inputs = [[1,2,3,2.5],[2,5,-1,2],[-1.5,2.7,3.3,-.8]]
